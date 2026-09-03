e2b740282f40ef3c059c771d820c02604eca0b2a
jeonseongkweon-cloud <jeonseongkweon@gmail.com>
2026-09-03 10:30:17 +0900

    chore: trigger A01 v1.4.0 deployment

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
  * 손안의 마법사 A01 저장소 설정
  * Supabase Project Settings > API 에서 아래 두 값을 입력하세요.
  * anon key는 브라우저 공개용 키이며, service_role 키는 절대 넣지 마세요.
+ * deploy trigger: 2026-09-03 10:28 KST
  */
 window.HAND_WIZARD_CONFIG = {
   supabaseUrl: '',
